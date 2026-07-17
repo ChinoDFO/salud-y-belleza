92881b51ee7e4e4aa739ac40379f410baa9461a8
Daniel Flores <dfo.floresortiz@gmail.com>
2026-07-16 23:27:16 -0600

    Probable diseño

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import logo_Salud_y_Belleza_Blanco from './logo_salud_y_belleza_blanco.png';
-//import { Fondo } from './components/Fondo';
 
 function App() {
   return (
-    <div className="App">
-        <section className="App-logo">
-          <img src={logo_Salud_y_Belleza_Blanco} className="App-logo2" alt="logo" />
-        </section>
+    <div className="hero">
+        {/* Usamos una sola clase contenedora para el espaciado del Logo */}
+          <section> 
+            <div className="Logo-container">
+            <img src={logo_Salud_y_Belleza_Blanco} className="App-logo2" alt="logo" />
+          </div>
+          </section>
+        {/* Primera sección de texto */}
         <section className="Info">
           <p> 
             Bienvenido a Salud y Belleza, la mejor tienda de productos de belleza 
             y cuidado personal.  
           </p>
         </section>
-        <section className="Info">
+
+        {/* Segunda sección de texto con margen extra para permitir scroll */}
+        <section className="Info Info-last">
           <p>
             En nuestra tienda encontrarás una amplia variedad de productos de alta calidad para el cuidado de tu piel, cabello y cuerpo.
           </p>
         </section>
-      
-    </div>
+        </div>
   );
 }
 
